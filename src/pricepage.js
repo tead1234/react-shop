18074f231a566567d1fe6a5659d643cfc04bfe7b
tead1234 <tead1234@gmail.com>
2022-07-10 00:02:46 +0900

    일단성공

--- src/pricepage.js
+++ src/pricepage.js
@@ -1,16 +1,18 @@
 import { Link, Outlet, useNavigate } from 'react-router-dom';
 import './pricepage.css'
 function Pricepage(props){
+    const nav = useNavigate();
     return(
         <>
         <div className='pricepage-price'>
             {
                 props.imgSrc.map((a,i)=>(
                     <img src={process.env.PUBLIC_URL+ a} 
-                        onClick={()=> <Link to='./pricing/datejust'></Link> }
+                        onClick={()=> nav('./datejust') }
                         style={{width:'400px', padding:'10px', paddingTop: "40px", display:'inline'}}></img> 
-                    ))         
+                    ))        
             }
+            <Outlet/> 
         </div>
         </>
         
