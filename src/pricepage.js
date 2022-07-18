7acc6ddca8abcd1c26c64a62969f42a306939e7e
tead1234 <tead1234@gmail.com>
2022-07-18 23:03:57 +0900

    use이펙트 상태주기 활용법

--- src/pricepage.js
+++ src/pricepage.js
@@ -10,18 +10,28 @@ function Pricepage(props){
         copy.sort()
         props.watch.setwatch(copy)
     }
-    let timer = ()=>{ setTimeout(()=>{
-        setalert(false)
-    }, 2000)
+    
 
-    }
+   
     // 컴포넌트의 주기 중 mount, update시 실행
     // html랜더링 이후 useEffect가 이어서 작동됨
     // 시간이 걸리는 연산, 데이터 가져오기 타이머
+    // 실행조건을 넣는 곳 []
+    // 마운트 시에만 작동시키고 싶으면 []만 넣으면 ㅇㅋ
+    // 숙제 input창에 숫자말고 이상한거 들어오면 알럿
     useEffect(()=>{
-        timer()
-    })
-   
+        let timer = ()=>{ setTimeout(()=>{
+            setalert(false)
+        }, 2000)
+        }
+        timer();
+        return () =>{
+            // unmount시랑 update만 작동됨
+            clearTimeout(timer);
+        }
+    },[])
+    // return clean up 함수 == > 재랜더링이 계속되니깐 기존 코드 정리용 함수
+    
     return(
         <>
         {
